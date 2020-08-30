76169a18663710156bb35cd3b233f48dbd762789
anupam-tiwari <appleanupam@gmail.com>
2020-08-30 22:47:43 +0530

    first

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_fMgQ3oySX', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3eaen7svjcdo7n38hrkafi6a2e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://cilbbexe52.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
---
+++ js/configmentor.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_xc9ZiYm39', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'v3bj7bn4gjt31u1gvvdfj4lf4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv v3bj7bn4gjt31u1gvvdfj4lf4
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://cilbbexe52.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
